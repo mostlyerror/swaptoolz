1392bc0a746c025877de69c796a40e30ed19a628
Benjamin Poon <benjamintpoon@gmail.com>
2021-01-17 15:08:08 -0700

    setting up tests controller & views

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,5 @@
+class TestsController < ApplicationController
+  def index; end
+  def hello_react; end
+  def photo; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
 
-  get 'test_photo' => 'home#test_photo'
+  get 'tests' => 'tests#index'
+  get 'tests/hello_react' => 'tests#hello_react'
+  get 'tests/photo' => 'tests#photo'
 end
