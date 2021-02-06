0a021a2cc128861e411a50ec1672a3fa63760a2a
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-06 15:26:45 -0700

    more edge cases for motel#participating?

--- test/models/motel_test.rb
+++ test/models/motel_test.rb
@@ -1,12 +1,19 @@
 require 'test_helper'
 
 class MotelTest < ActiveSupport::TestCase
-  test "#participating? returns true if some room availability has been created" do
+  test "#participating? returns true if some room availability" do
     motel = Motel.create!(name: 'asdf', address: 'asdf', phone: 'asdf')
-    swe = SevereWeatherEvent.create!(start_date: Date.today, end_date: Date.tomorrow)
+    swe = SevereWeatherEvent.create(start_date: Date.today, end_date: Date.tomorrow)
     refute motel.participating?(swe)
 
-    motel.availabilities.create!(severe_weather_event: swe, rooms: 10)
+    motel.availabilities.create(severe_weather_event: swe, rooms: 0)
+    refute motel.participating?(swe)
+
+    motel.availabilities.create(severe_weather_event: swe, rooms: 10)
+    refute motel.participating?(swe)
+
+    motel.availabilities.destroy_all
+    motel.availabilities.create(severe_weather_event: swe, rooms: 10)
     assert motel.participating?(swe)
   end
 end
