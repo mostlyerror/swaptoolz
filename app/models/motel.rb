283c9407193ad0fd01917ba6cc0fbbb1aacde96f
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-06 15:41:09 -0700

    testing motel participation and indication of participation

--- app/models/motel.rb
+++ app/models/motel.rb
@@ -5,10 +5,11 @@ class Motel < ApplicationRecord
     has_many :availabilities
     # has_many :severe_weather_events, through: :availabilities
 
+    def participation_indicated?(swe)
+      Availability.exists?(motel: self, severe_weather_event: swe)
+    end
+
     def participating?(swe)
-      rooms = Availability.where(motel: self, severe_weather_event: swe)
-        .first
-        &.rooms
-      rooms.to_i > 0
+      Availability.find_by(motel: self, severe_weather_event: swe)&.rooms.to_i > 0
     end
 end
