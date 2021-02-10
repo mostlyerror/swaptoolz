3f78b9be6201cf4f5e4ea6a3ce4368e3b5c6952e
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-10 16:38:48 -0700

    attempt to fix missing `roles` association

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @swe = SevereWeatherEvent.current
+    
     if current_user.is_admin?
       render plain: 'admin'
-      if current_user.is_motel?
-        render plain: 'motel'
-      end
+    elsif current_user.is_motel?
+      render plain: 'motel'
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def is_admin?
-    roles_name.include? "admin"
+    self.roles_name.include? "admin"
   end
 
   def is_motel?
-    roles_name.include? "front_desk"
+    self.roles_name.include? "front_desk"
   end
 end
