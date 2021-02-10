dca99ed4bcf1b7e580404093942ae3d72d4995ec
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-10 16:31:36 -0700

    use user type/role predicates in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
     @swe = SevereWeatherEvent.current
-    if current_user.roles_name.include? 'admin'
+    if current_user.is_admin?
       render plain: 'admin'
-    elsif current_user.roles_name.include? 'front_desk'
-      render plain: 'motel'
+      if current_user.is_motel?
+        render plain: 'motel'
+      end
     end
   end
 end
