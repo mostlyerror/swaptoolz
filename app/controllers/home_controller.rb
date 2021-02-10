fcd97fbb2fcd7e43ebf2366bdab163eb9347c85b
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-10 04:20:52 -0700

    fix broken system test cause of stupid rubocop help message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,3 @@ class HomeController < ApplicationController
     @participation = current_user.motel.participating?(@swe)
   end
 end
-
-Tip: Based on detected gems, the following RuboCop extension libraries might be helpful:
-  * rubocop-rails (http://github.com/rubocop-hq/rubocop-rails)
-
-You can opt out of this message by adding the following to your config (see https://docs.rubocop.org/rubocop/extensions.html#extension-suggestions for more options):
-  AllCops:
-    SuggestExtensions: false
