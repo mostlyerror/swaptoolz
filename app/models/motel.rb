389dccf8dd9167d08591259bd0744b5391eeaa71
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-10 04:18:17 -0700

    cleanup some react experiments, remove direct link between user and motel model

--- app/models/motel.rb
+++ app/models/motel.rb
@@ -1,16 +1,15 @@
 class Motel < ApplicationRecord
-    validates_presence_of :name, :address, :phone
+  validates_presence_of :name, :address, :phone
 
-    has_many :users
-    has_many :availabilities
-    # has_many :severe_weather_events, through: :availabilities
+  has_many :availabilities
 
-    def participation_indicated?(swe)
-      Availability.exists?(motel: self, severe_weather_event: swe)
-    end
+  def participation_indicated?(swe)
+    Availability.exists?(motel: self, severe_weather_event: swe)
+  end
 
-    def participating?(swe)
-      return false if swe.nil?
-      Availability.find_by(motel: self, severe_weather_event: swe)&.rooms.to_i > 0
-    end
+  def participating?(swe)
+    return false if swe.nil?
+
+    Availability.find_by(motel: self, severe_weather_event: swe)&.rooms.to_i > 0
+  end
 end
--- app/models/voucher_application.rb
+++
@@ -1,2 +0,0 @@
-class VoucherApplication < ApplicationRecord
-end
