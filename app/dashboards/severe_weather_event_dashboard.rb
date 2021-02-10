f1cec1b8ddf20a15c0b172983cc25251458ffbbc
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-10 10:55:33 -0700

    cleaning up users+motels for administrate dashboards

--- app/dashboards/severe_weather_event_dashboard.rb
+++ app/dashboards/severe_weather_event_dashboard.rb
@@ -8,7 +8,6 @@ class SevereWeatherEventDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    voucher_applications: Field::HasMany,
     availabilities: Field::HasMany,
     id: Field::Number,
     start_date: Field::Date,
@@ -23,7 +22,6 @@ class SevereWeatherEventDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  voucher_applications
   availabilities
   id
   start_date
@@ -32,7 +30,6 @@ class SevereWeatherEventDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  voucher_applications
   availabilities
   id
   start_date
@@ -45,7 +42,6 @@ class SevereWeatherEventDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  voucher_applications
   availabilities
   start_date
   end_date
--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -8,7 +8,7 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    motel: Field::BelongsTo,
+    roles: Field::HasMany,
     id: Field::Number,
     email: Field::String,
     encrypted_password: Field::String,
@@ -25,8 +25,8 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  motel
   id
+  roles
   email
   encrypted_password
   ].freeze
@@ -34,8 +34,8 @@ class UserDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  motel
   id
+  roles
   email
   encrypted_password
   reset_password_token
@@ -49,7 +49,7 @@ class UserDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  motel
+  roles
   email
   encrypted_password
   reset_password_token
